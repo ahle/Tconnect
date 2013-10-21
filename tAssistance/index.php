75773ee97df21282074af9a1027ac85a3f677ae2
ahle <hisokaxmagician@gmail.com>
2013-10-21 17:27:22 +0200

    new interface of assistance

--- tAssistance/index.php
+++ tAssistance/index.php
@@ -1,6 +1,6 @@
 <?php
 $dir = dirname(__FILE__);
-require_once $dir.'/includes/global_variable.php';
+require_once $dir.'/includes/global.php';
 
 function get_user($session_id){
 	global $db_server,$db_user,$db_pwd,$db_name;
@@ -26,8 +26,12 @@ $session_id = session_id();
 $user_id = get_user($session_id);
 
 if($_GET["page"]=="TraceView"){
+	global $base_uri;
+	$trace_uri = $base_uri."trc_".$user_id."/";
+	
 	$page = file_get_contents($root_dir."/html/Trace.html");
 	$page = str_replace("\$user_id", $user_id, $page);
+	$page = str_replace("\$trace_uri", $trace_uri, $page);
 	
 	echo $page;
 	exit;
@@ -38,6 +42,39 @@ if($_GET["page"]=="EditStyle"){
 	$page = file_get_contents($root_dir."/html/EditStyle.html");
 	$page = str_replace("\$style_id", $style_id, $page);
 	
+	echo $page;
+	exit;
+}
+
+if($_GET["page"]=="Property"){
+	$obsel_id = $_GET["id"];
+	$obsel_str = $_GET["obsel"];
+	$obsel = json_decode($obsel_str);
+		
+	//$page = file_get_contents($root_dir."/html/Property.html");
+	//echo $obsel;
+	$page = "<div class='table-responsive'>";
+	$page.= "<table class='table table-striped' style='padding: 3px;'>";
+	foreach($obsel as $p => $o){
+		if($p=="begin"||$p=="end"){
+			$o = gmdate("c",$o/1000);
+		}
+		
+		if($p=="m:idSession"){
+		#	$page.= "<code data-toggle='tooltip' title='".htmlentities($o)."' data_placement='right'>session</code> ";
+		}
+		elseif($p=="m:idDoc"){
+		#	$page.= "<code data-toggle='tooltip' title='".htmlentities($o)."' data_placement='right'>doc</code> ";
+		}
+		#else{
+		if(true){
+			$page.= "<tr><td style='font-size: 12px'><code>".htmlentities($p)."</code></td><td style='font-size: 12px'>".htmlentities($o)."</td></tr>";	
+		}
+	}
+	$page.= "</div>";
+	
+	//$page = str_replace("\$style_id", $style_id, $page);
+
 	echo $page;
 	exit;
 }
--- tAssistance/js/tAssistance.js
+++ tAssistance/js/tAssistance.js
@@ -13,22 +13,16 @@
 	 * @name obsel_serializeHtml
 	 * @param options.obsel the obsel needed to be serialized in html
 	 * @desc format the obsel in html */	
-	tAssistance.obsel_serializeHtml = function(options){
+	tAssistance.obsel_property = function(options){
 		var obsel = options.obsel;
+		var obsel_container = options.container;
+		var obsel_str = JSON.stringify(obsel);
 		
-		var tableHtml = "<table>";
-    	for(var p in obsel){
-    		if(obsel.hasOwnProperty(p)){
-    			var o = obsel[p];
-    			if(p=="begin"||p=="end"){
-    				o = (new Date(o)).toUTCString();
-    			}
-    			var rowHtml = "<tr><td>"+p+"</td><td>"+o+"</td></tr>";
-    			tableHtml+= rowHtml;
-    		}
-    	}
-    	tableHtml+="</table>";
-		return tableHtml;
+		$.get("index.php?page=Property&id=1&obsel="+encodeURIComponent(obsel_str),function(data){
+			
+			$(obsel_container).empty().append(data);
+			$(".trace_property").find("code").tooltip();
+		});
 	};
 	
 	/** 
@@ -51,7 +45,7 @@
 			return d["subject"];
 		},
 		getSymbol = function(d){
-			return tAssistance.location+"img/ozem_fn.png";
+			return "img/ozem_fn.png";
 		},
 		margin = {top: 5.5, right: 19.5, bottom: 80, left: 60},
 		timeticks = [[d3.time.days,1,"%Y-%b-%d"],[d3.time.hours,4,"%Y-%b-%d %H"]],
@@ -79,7 +73,7 @@
 			return Math.floor(Math.random()*100);
 		},
 		tracePanelTmpl = function(id, source){
-			return "<table id='"+id+"'><tr><td class='trace_grid'></td><td style='font-family: arial; font-size: small'>Properties<div class='trace_property' style=' overflow: auto; width:200px'></div></td></tr>"
+			return "<table id='"+id+"'><tr><td class='trace_grid'></td><td style='font-family: arial; font-size: small'>Properties<div class='trace_property' style='width:200px'></div></td></tr>"
     		//+"<tr><td colspan='2' style='font-family: arial; font-size: small'>x: <span class='trans_x'>0</span> y:<span class='trans_y'>0</span> scale: <span class='scale'>0</span> source: <span class='source'>"+source+"</span></td></tr>"
     		//+"<tr><td colspan='2' style='font-family: arial; font-size: small'>Command <input type='input' class='cmd_input' /><button class='bt_run' >Run</button></td></tr></table>"
 			+"</table>";
@@ -200,11 +194,10 @@
         })
         .on("click", function(d,i){// add click event on obsel
         					        	
-        	var obsel_in_html = tAssistant.obsel_serializeHtml({obsel: d});
-        	
-        	//$("#"+rootid+" .trace_property").empty().append(obsel_in_html);
-        	
-        	
+        	var obsel_in_html = tAssistance.obsel_property({
+        		"obsel": d, 
+        		"container": "#"+rootid+" .trace_property"
+        		});
         });
 	    
 	    
@@ -219,34 +212,128 @@
 		
 	};
 	
-	/*tAssistance.draw_obsels1 = function(options){
-		var obsels = options.obsels,
-		css_rules = options.css_rules !== undefined ? options.css_rules : 'g_obsels_enter.append("svg:image").attr("class", "obsel").attr("xlink:href",function(d){return getSymbol(d);}).attr("width","20").attr("height","20");',
-		parentNode = options.parentNode,
-		source = options.source,
-		getx = function(d){
-			var date = new Date(d["begin"]);
-			return date;
-		},
-		gety = options.gety !== undefined ? options.gety : function(d){ 
-			return d["subject"];
-		},
-		getSymbol = function(d){
-			return "/tracestore/tAssistant/img/ozem_fn.png";
-		},
-		margin = {top: 5.5, right: 19.5, bottom: 80, left: 60},
-		timeticks = [[d3.time.days,1,"%Y-%b-%d"],[d3.time.hours,4,"%Y-%b-%d %H"]],
-		width = 600;
+	tAssistance.draw_obsels1 = function(options){
+		var obsels = options.obsels;
+		
+		// make a svg
+		var svgNS = "http://www.w3.org/2000/svg";
+
+		var svg = document.createElementNS(svgNS,"svg");
+		svg.setAttribute("version","1.2");
+		
+		document.body.appendChild(svg);
+		
+		myCircle = document.createElementNS(svgNS,"circle");
+		myCircle.setAttributeNS(null,"cx",100);
+		myCircle.setAttributeNS(null,"cy",100);
+		myCircle.setAttributeNS(null,"r",10);
+		myCircle.setAttributeNS(null,"fill","none");
+		myCircle.setAttributeNS(null,"stroke","black");
+		
+		svg.appendChild(myCircle);
 		
 		
 		
-	};
+	}
 	
-	tAssistant.config_read = function(options){
+	tAssistance.draw_obsels2 = function(opts){
+		var g = opts.g,
+		positions = opts.positions;
 		
+		// make a svg
+		var svgNS = "http://www.w3.org/2000/svg";
+
+		//var svg = document.createElementNS(svgNS,"svg");
+		//svg.setAttribute("version","1.2");
+				
+		//document.body.appendChild(svg);
+		$.each(positions, function(index, position){
+			color = '#'+Math.floor(Math.random()*16777215).toString(16);
 		
+			myCircle = document.createElementNS(svgNS,"circle");
+			myCircle.setAttributeNS(null,"cx",position.x);
+			myCircle.setAttributeNS(null,"cy",position.y);
+			myCircle.setAttributeNS(null,"r",8);
+			myCircle.setAttributeNS(null,"fill",color);
+			myCircle.setAttributeNS(null,"stroke","black");
+			
+			g.appendChild(myCircle);
+
+			// attach the event
+			$(myCircle).click(function(e){
+				var obsel_in_html = tAssistance.obsel_property({
+		    		"obsel": position.source_obsel, 
+		    		"container": "#controlPanel"
+		    		});
+			});
+					
+		});
 		
-	}*/
+	}
+	
+	tAssistance.position_obsels = function(obsels, opts){
+		var x_unit = opts.x_unit,
+		x0 = opts.x0,
+		y0 = opts.y0;
+		
+		// calculate the positions of obsels based on x0,y0
+		var result = [];
+		$.each(obsels, function(index, obsel){
+			item = new Object();
+			item.x = index*x_unit+x0;
+			item.y = y0;
+			item.source_obsel = obsel;
+			
+			result.push(item);
+		});
+		
+		return result;		
+	}
+	
+	/* Constants: */
+    var leftArrow  = 37;	// Key code for the left arrow key.
+    var upArrow    = 38;
+    var rightArrow = 39;
+    var downArrow  = 40;
+    var panRate    = 10;	// Number of pixels to pan per key press.
+	
+	tAssistance.processKeyPress = function(g,evt){
+      var transform = g.getAttribute('transform');	// Grab the object representing the SVG element's viewBox attribute.
+      var translate = transform.split(' ')[0];
+      var translateValues = translate.replace("translate(","").slice(0,-1).split(',');// Create an array and insert each individual view box attribute value (assume they're seperated by a single whitespace character).
+      var scale = transform.split(' ')[1];
+      var scaleValues = scale.replace("scale(","").slice(0,-1).split(',');
+      
+      /* The array is filled with strings, convert the first two viewBox values to floats: */
+      translateValues[0] = parseFloat(translateValues[0]);	// Represent the x-coordinate on the viewBox attribute.
+      translateValues[1] = parseFloat(translateValues[1]);	// Represent the y coordinate on the viewBox attribute.
+      
+      scaleValues[0] = parseFloat(scaleValues[0]);
+      scaleValues[1] = parseFloat(scaleValues[1]);
+      
+      switch (evt.keyCode)
+      {
+        case leftArrow:
+        	translateValues[0] += panRate;	// Increase the x-coordinate value of the viewBox attribute by the amount given by panRate.
+          break;
+        case rightArrow:
+        	translateValues[0] -= panRate;	// Decrease the x-coordinate value of the viewBox attribute by the amount given by panRate.
+          break;
+        case upArrow:
+        	scaleValues[0] += 0.25;	// Increase the y-coordinate value of the viewBox attribute by the amount given by panRate.          break;          
+          break;
+        case downArrow:
+        	scaleValues[0] -= 0.25;	// Decrease the y-coordinate value of the viewBox attribute by the amount given by panRate.          break;          
+          break;               
+      } // switch
+      
+      g.setAttribute('transform', "translate("+translateValues.join(',')+") scale("+scaleValues.join(',')+")");	// Convert the viewBoxValues array into a string with a white space character between the given values.
+    }
+	
+	var isDown = false;
+	var startCoords = {};
+	//var last = [0, 0];
+	
 	/** 
 	 * @function
 	 * @memberof tAssistance
@@ -289,11 +376,130 @@
 				
 				console.log("test trace_read correctly");
 				
-				var widget = new tAssistance.draw_obsels({
-					"obsels": obsels,
-					"source": trace_uri,
-					"parentNode": document.getElementById("tracePanel")
-				});			
+				//var widget = new tAssistance.draw_obsels1({
+				//	"obsels": obsels,
+				//	"source": trace_uri,
+				//	"parentNode": document.getElementById("tracePanel")
+				//});
+				opts = {
+						x_unit: 20,
+						x0: 0,
+						y0: 10
+				};
+				
+				// make a svg
+				var svgNS = "http://www.w3.org/2000/svg";
+
+				var svg = document.createElementNS(svgNS,"svg");
+				svg.setAttribute("version","1.2");
+				svg.setAttribute("style","overflow: auto");
+				svg.setAttribute("viewBox","0 0 1000 300");
+				svg.setAttribute("draggable","false");
+				svg.setAttribute("width","1000px");
+				svg.setAttribute("height","300px");
+				
+				$("#chart1").get(0).appendChild(svg);
+				
+				var rect = document.createElementNS(svgNS,"rect");
+				//rect.setAttribute("style","background-color: yellow");
+				rect.setAttribute("fill","yellow");
+				rect.setAttribute("width","100%");
+				rect.setAttribute("height","100%");
+				
+				svg.appendChild(rect);
+				
+				var g = document.createElementNS(svgNS,"g");				
+				g.setAttribute("transform","translate(0,0) scale(1,1)");
+				svg.appendChild(g);
+				
+				
+				
+				positions = tAssistance.position_obsels(obsels, opts);
+				//console.log(positions);
+				
+				positions = tAssistance.draw_obsels2({"g": g,"positions": positions});
+				//console.log(positions);
+				
+				/* Add event listeners:  */
+			    $(document).keydown(function(e){
+			    	tAssistance.processKeyPress(g,e);
+			    });
+			    
+			    $(rect).mousedown(function(e) {
+			        isDown = true;
+			        e.preventDefault();
+			        
+			        var transform = g.getAttribute('transform');	// Grab the object representing the SVG element's viewBox attribute.
+			        var translate = transform.split(' ')[0];
+			        var translateValues = translate.replace("translate(","").slice(0,-1).split(',');// Create an array and insert each individual view box attribute value (assume they're seperated by a single whitespace character).
+			        var scale = transform.split(' ')[1];
+			        var scaleValues = scale.replace("scale(","").slice(0,-1).split(',');	
+			        
+			        /* The array is filled with strings, convert the first two viewBox values to floats: */
+			        translateValues[0] = parseFloat(translateValues[0]);	// Represent the x-coordinate on the viewBox attribute.
+			        translateValues[1] = parseFloat(translateValues[1]);	// Represent the y coordinate on the viewBox attribute.
+			        
+			        scaleValues[0] = parseFloat(scaleValues[0]);
+			        scaleValues[1] = parseFloat(scaleValues[1]);
+			        
+			        //console.log(startCoords);
+			        startCoords = {
+			            "mouseDown.x": e.offsetX,
+			            "mouseDown.y": e.offsetY,
+			            //e.offsetY - last[1]
+			            "group.x": translateValues[0],
+			            "group.y": translateValues[1]
+			        	};
+			    });
+			    
+			    $(rect).mouseup(function(e) {
+			        isDown = false;
+			        e.preventDefault();
+			        //console.log("mouse up");
+			       /* last = [
+			            e.offsetX - startCoords[0], // set last coordinates
+			            //e.offsetY - startCoords[1]
+			            startCoords[1]
+			        ];*/
+			    });
+
+			    $(rect).mousemove(function(e){
+			    	e.preventDefault();
+			        if(!isDown) return;
+			        //console.log("panning");
+			        var x = e.offsetX;
+			        var y = e.offsetY;
+			        //var _x = 
+			        	
+			        var transform = g.getAttribute('transform');	// Grab the object representing the SVG element's viewBox attribute.
+			        var translate = transform.split(' ')[0];
+			        var translateValues = translate.replace("translate(","").slice(0,-1).split(',');// Create an array and insert each individual view box attribute value (assume they're seperated by a single whitespace character).
+			        var scale = transform.split(' ')[1];
+			        var scaleValues = scale.replace("scale(","").slice(0,-1).split(',');	
+			        
+			        /* The array is filled with strings, convert the first two viewBox values to floats: */
+			        translateValues[0] = parseFloat(translateValues[0]);	// Represent the x-coordinate on the viewBox attribute.
+			        translateValues[1] = parseFloat(translateValues[1]);	// Represent the y coordinate on the viewBox attribute.
+			        
+			        scaleValues[0] = parseFloat(scaleValues[0]);
+			        scaleValues[1] = parseFloat(scaleValues[1]);
+			        
+			        // transform
+			        translateValues[0] = x - startCoords["mouseDown.x"] + startCoords["group.x"];
+			        //translateValues[1] = y - startCoords["mouseDown.y"] + startCoords["group.y"];
+			        translateValues[1] = startCoords["group.y"];
+			        
+			        //console.log(startCoords);
+			        //this.(1, 0, 0, 1,
+			        //                 x - startCoords[0], y - startCoords[1]);
+			        //g.setAttributeNS(null,"transform", "translate("+ (x - startCoords[0]) +","+ (startCoords[1]) +")");
+			        g.setAttribute('transform', "translate("+translateValues.join(',')+") scale("+scaleValues.join(',')+")");	
+			        
+			        
+			        //render();
+			    });
+
+				
 			}
 		});		
 	}
