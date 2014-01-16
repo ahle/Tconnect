030e4e6ac70972d3c007828520fa6b60efbd2a0f
Anh Hoang Le <hisokaxmagician@gmail.com>
2014-01-16 11:01:52 +0100

    Update tService.js
    change the success and error functions to an only callback function

--- tService/js/tService.js
+++ tService/js/tService.js
@@ -8,29 +8,114 @@ tService = {};
  * @namespace tService
  * @name TraceManager
  * @constructor 
- * @param options.base_uri 	URI of the base de traces
- * @param options.async 	The synchonized mode
- * @desc a tool to manage traces in a base de traces */
+ * @param options.root_uri 	The root URI of the trace base management system
+ * @param options.async 	The synchonized mode. The request is called in asynchronous mode by default.
+ * @desc a proxy interface to communicate with a trace base management system (TBMS). The KTBS by default */
 tService.TraceManager = function(options){
-	this.base_uri = options.base_uri;
+	this.root_uri = options.root_uri;
 	this.async = options.async ? options.async : true;
+	
+	/** 
+	 * @function
+	 * @memberof TraceManager#
+	 * @name init_base
+	 * @param t_options.name the name of the base
+	 * @desc initialize a remote base */
+	this.init_base = function(t_options){
+		var opt = {};
+		opt.root_uri = this.root_uri;
+		opt.async = this.async;
+		opt.name = t_options.name;
+		var trace = new tService.Base(opt);
+		return trace;
+	}
 	
 	/** 
 	 * @function
 	 * @memberof TraceManager#
 	 * @name init_trace
 	 * @param t_options.name the name of the trace
-	 * @desc initialize an remote trace */
+	 * @desc initialize a remote trace */
 	this.init_trace = function(t_options){
 				
 		var opt = {};
-		opt.base_uri = this.base_uri;		
+		opt.base_uri = this.base_uri;
 		opt.async = this.async;
 		opt.name = t_options.name;
 		var trace = new tService.Trace(opt);
 		return trace;
 	}
 }
+
+/** 
+ * @name Base
+ * @constructor 
+ * @param options.base_uri 	URI of the root 
+ * @param options.name 		the name of the trace
+ * @param options.async 	The synchonized mode
+ * @desc a generic trace */
+tService.Base = function(options){
+	//this.trace_manager = options.trace_manager;
+	this.root_uri = options.root_uri;
+	this.name = options.name; 
+	//this.success = options.success,
+	//this.error = options.error,
+	this.async = options.async ? options.async : true;
+	this.base_uri = this.root_uri+this.name+"/";
+	/** 
+	 * @function
+	 * @memberof Trace#
+	 * @name send
+	 * @param s_options.callback the callback function
+	 * @desc send the base information to the TBMS */
+	this.send = function(s_options){
+		
+		var root_uri = this.root_uri,
+		name = this.name,
+		callback = s_options.callback,
+		async = this.async;
+
+		function base2Turtle(name,root_uri){
+			
+			var id = ktbs_base_uri + name;
+			
+			var prefixes = [];
+			prefixes.push("@prefix xsd: <http://www.w3.org/2001/XMLSchema#> .");
+			prefixes.push("@prefix : <http://liris.cnrs.fr/silex/2009/ktbs#> .");
+			
+			var statements = [];
+			statements.push("<> :hasBase <"+id+"/>.");
+			statements.push("<"+id+"/> a :Base.");
+			statements.push("<"+id+"/> rdfs:label \""+name+"\" .");
+			
+			// TODO
+			
+			return prefixes.join("\n")+"\n"+statements.join("\n");
+		}
+
+		var ctype = "text/turtle";
+
+		var base_in_turtle = base2Turtle(name,root_uri);
+		
+		// post to ktbs
+		jQuery.ajax({
+			url: root_uri,
+			type: 'POST',
+			data: base_in_turtle,
+			contentType: ctype,
+			crossDomain: true,
+			success: function(ret){
+					console.log("The base ["+name+"] is created successfully!");
+					callback("success",ret);
+				},
+			error: function(jqXHR, textStatus, errorThrown){
+					
+					console.log("E: The base ["+name+"] cannot be created.");
+					callback("error", jqXHR, textStatus, errorThrown);
+				},
+			async: async
+		});
+	}
 
 /** 
  * @name Trace
@@ -40,28 +125,23 @@ tService.TraceManager = function(options){
  * @param options.async 	The synchonized mode
  * @desc a generic trace */
 tService.Trace = function(options){
-	//this.trace_manager = options.trace_manager;
 	this.base_uri = options.base_uri;
 	this.name = options.name; 
-	//this.success = options.success,
-	//this.error = options.error,
 	this.async = options.async ? options.async : true;
-	this.model_name = "model1";
+	this.model_name = "model1";// hardcode
 	this.model_uri = this.base_uri+this.model_name+"/";
 	this.trace_uri = this.base_uri+this.name+"/";
 	/** 
 	 * @function
 	 * @memberof Trace#
-	 * @name save
-	 * @param s_options.success the callback function if the save is successful
-	 * @param s_options.error the callback function if the save is failed
+	 * @name send
+	 * @param s_options.callback the callback function
 	 * @desc save the trace into the base de traces */
-	this.save = function(s_options){
+	this.send = function(s_options){
 		
 		var base_uri = this.base_uri,
 		name = this.name,
-		successCallback = s_options.success,
-		errorCallback = s_options.error,
+		callback = s_options.callback,
 		async = this.async,
 		model_name = this.model_name;
 
@@ -100,15 +180,15 @@ tService.Trace = function(options){
 			crossDomain: true,
 			success: function(ret){
 					console.log("The trace ["+name+"] is created successfully!");
-					successCallback(ret);
+					callback("success", ret);
 				},
 			error: function(jqXHR, textStatus, errorThrown){
 					
 					console.log("E: The trace ["+name+"] cannot be created.");
-					errorCallback(jqXHR, textStatus, errorThrown);
+					callback("error", jqXHR, textStatus, errorThrown);
 				},
 			async: async
-		});		
+		});
 	}
 	
 	/** 
@@ -175,12 +255,12 @@ tService.Trace = function(options){
 			crossDomain: true,
 			success: function(ret){
 					console.log("The obsel ["+obsel["id_ktbs"]+"] is sent successfully!");
-					successCallback(ret);
+					callback("success", ret);
 				},
 			error: function(jqXHR, textStatus, errorThrown){
 					
 					console.log("E: The obsel ["+obsel["id_ktbs"]+"] cannot be sent.");
-					errorCallback(jqXHR, textStatus, errorThrown);
+					callback("error", jqXHR, textStatus, errorThrown);
 				},
 			async: async
 		});
@@ -190,14 +270,12 @@ tService.Trace = function(options){
 	 * @function
 	 * @memberof Trace#
 	 * @name get_obsels
-	 * @param s_options.success 	the callback function if the save is successful
-	 * @param s_options.error	 	the callback function if the save is failed
+	 * @param s_options.callback 	the callback function
 	 * @param s_options.nocache 	no use the cached result if nocache = true
 	 * @desc put obsels into the trace in the base de traces */	
 	this.get_obsels = function(s_options){
 		var trace_uri = this.trace_uri,
-		successCallback = s_options.success,
-		errorCallback = s_options.error,
+		callback = s_options.callback,
 		nocache = s_options.nocache || false,
 		async = this.async;
 		
@@ -214,7 +292,7 @@ tService.Trace = function(options){
 			$.each(obsels, function(obsel){
 				// TODO
 			});				
-			successCallback(obsels);
+			callback(obsels);
 		});
 	}
 }
