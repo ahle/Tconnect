0dd225b38ce9673374bb1e5e284407df127b5a35
ahle <ahle@ahle-VirtualBox.(none)>
2014-06-02 15:18:43 +0200

    fix bugs

--- project/Ozalid/TStore/php/OzaVWCounter.php
+++ project/Ozalid/TStore/php/OzaVWCounter.php
@@ -7,7 +7,7 @@ class OzaVWCounter{
 	}
 	
 	function receiveObsel($new_obsel){
-		require_once "/var/www/tconnect/project/Ozalid/TStore/php/OzaTStore.php";
+		require_once "OzaTStore.php";
 		
 		$trace = $this->trace;
 		
