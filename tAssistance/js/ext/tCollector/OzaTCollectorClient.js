8a95fcc97113491fcb64191aec62c777f737d26d
ahle <ahle@ahle-VirtualBox.(none)>
2015-03-03 15:28:29 +0100

    configuration page

--- tAssistance/js/ext/tCollector/OzaTCollectorClient.js
+++ tAssistance/js/ext/tCollector/OzaTCollectorClient.js
@@ -1,5 +1,6 @@
 tCollector.OzaTCollectorClient = function(){
-	this.uri = "http://localhost/tconnect/project/Ozalid/TraceCollector/";
+	//this.uri = "http://localhost/tconnect/project/Ozalid/TraceCollector/"; // use this line for local dev
+	this.uri = "http://dsi-liris-silex.univ-lyon1.fr/ozalid/tconnect/project/Ozalid/TraceCollector/";// use this line for git
 	
 	this.getIndexPageUri = function(){
 		return this.uri+"index.php";
